44083354fefb89ab87583bdeb53bfc8631d417c9
PrimalPancake92 <johnle.jl39@gmail.com>
2024-11-28 13:07:23 +1100

    Updated the book class to have some getter methods.

--- LibraryManagement/src/Book.java
+++ LibraryManagement/src/Book.java
@@ -1,7 +1,39 @@
 public class Book {
     private int bookId;
-    private String name;
     private String title;
     private String author;
+    private String publisher;
+    private int yearPublished;
+    private boolean borrowed;
     
+    public Book (int bookId, String title, String author){
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.borrowed = false;
+    }
+
+    public int getBookId(){
+        return bookId;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getPublisher(){
+        return publisher;
+    }
+
+    public int getYearPublished(){
+        return yearPublished;
+    }
+
+    public boolean getBorrowed(){
+        return borrowed;
+    }
 }
